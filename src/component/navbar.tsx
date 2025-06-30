9694a921ee2c0f0606526c55ba588e31c539fa5a
muniss950 <munish3971@gmail.com>
2025-06-30 15:20:21 +0530

    add more project

--- src/component/navbar.tsx
+++ src/component/navbar.tsx
@@ -6,7 +6,7 @@ import logo from '../logo.svg';
 
 function NavBar() {
   return (
-    <header className="flex justify-between items-center bg-[#222831] rounded-lg p-4 shadow-lg sticky top-0 z-50 h-24">
+    <header className="flex justify-between items-center bg-[#222831] rounded-lg p-4 shadow-lg sticky top-0 z-50 h-15">
       {/* Logo + Title */}
       <div className="flex items-center space-x-4">
         <img className="h-12 w-12" src={logo} alt="logo" />
